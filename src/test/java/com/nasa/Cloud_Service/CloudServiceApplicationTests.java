933919ab389f5508ae08a59d1541918a47575ce9
Sanjay Kumar <sajusanjay7092@gmail.com>
2026-03-22 19:47:22 +0530

    initial commit

---
+++ src/test/java/com/nasa/Cloud_Service/CloudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.Cloud_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
